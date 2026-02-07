69230f0f184995eaadea6098d640e058f678fcbf
陳國翔 <chen199940@gmail.com>
2026-02-08 00:43:51 +0800

    feat(config): update dependencies and refine settings for improved security and performance

--- ela_backend/ela_backend/settings.py
+++ ela_backend/ela_backend/settings.py
@@ -10,11 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 FROM_EMAIL = "ELA <support@mail.tonixiang.me>"
 SEND_EMAIL_API_KEY = os.getenv("SEND_EMAIL_API_KEY")
 
-SECRET_KEY = os.getenv("S", 'django-insecure-n(fc)j3oolawouvvm=zs5dw4z1=k5u1a+nd_79e73-n@+91xy4')
+SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-n(fc)j3oolawouvvm=zs5dw4z1=k5u1a+nd_79e73-n@+91xy4')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com']
+# .onreder.com 之後要移除，請統一用 api.tonixiang.me，防止攻擊者可能註冊類似的子域名進行攻擊
+ALLOWED_HOSTS = ['.onrender.com', 'api.tonixiang.me']
 
 APPEND_SLASH = True
 
@@ -22,9 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -35,33 +33,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'ela_backend.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'ela_backend.wsgi.application'
 
+# 目前系統使用 PostgreSQL 超級使用者，過高權限，之後使用最小權限原則配置資料庫使用者
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -95,8 +76,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -143,8 +122,7 @@ SIMPLE_JWT = {
 
 AUTH_USER_MODEL = 'auth_app.User'
 
-CORS_ALLOWED_ORIGINS = ["https://english-learning-assistant-backend-rxt3.onrender.com",
-                        "https://english-learning-assistant.pages.dev"]
+CORS_ALLOWED_ORIGINS = ["https://english-learning-assistant.pages.dev"]
 CORS_ALLOW_CREDENTIALS = True
 
 CACHES = {
--- ela_backend/ela_backend/urls.py
+++ ela_backend/ela_backend/urls.py
@@ -1,3 +1,4 @@
+# Main URL configuration for the ElA Backend project.
 from django.urls import path, include
 urlpatterns = [
     path("auth/", include("auth_app.urls"),name="auth_app"),
