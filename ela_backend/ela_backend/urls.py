ac55b0e72e49848a001d97bbfbdbf289666f97e6
陳國翔 <chen199940@gmail.com>
2026-02-20 10:06:59 +0800

    feat(app): add llm_app URL routing and clean up models.py

--- ela_backend/ela_backend/urls.py
+++ ela_backend/ela_backend/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 urlpatterns = [
     path("auth/", include("auth_app.urls"),name="auth_app"),
-    path("chat/", include("chat_app.urls"), name="chat_app")
+    path("chat/", include("chat_app.urls"), name="chat_app"),
+    path("llm/", include("llm_app.urls"), name="llm_app"),
 ]
 
